3257b54d482daa943f807de2098f1b672ff40e35
Ryan <keemink.ryan@gmail.com>
2021-12-23 22:19:43 +0100

    big changes

---
+++ code/Auto/include/Wifiserver.h
@@ -0,0 +1,173 @@
+#include <WiFi.h>
+
+const char* ssid = "VRV9517567E59";
+const char* password = "3naNqtr9f7V4";
+WiFiServer server(80);
+
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+
+// Auxiliar variables to store the current output state
+String drivestate = "automatic";
+String manualactionstate = "stopped";
+
+String header;
+
+
+void serverSetup(){
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+}
+
+
+void webserver(){
+   WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+      currentTime = millis();
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            
+              if (header.indexOf("GET /automatic") >= 0) {
+              drivestate = "automatic";
+              manualactionstate = "stopped";
+
+            } else if (header.indexOf("GET /manual") >= 0) {
+              drivestate = "manual";
+            }
+              if (header.indexOf("GET /stopped") >= 0) {
+              manualactionstate = "stopped"; 
+              }
+              else if (header.indexOf("GET /forward") >= 0) {
+              manualactionstate = "forward";
+              }
+              else if (header.indexOf("GET /backward") >= 0) {
+              manualactionstate = "backward";
+              }
+              else if (header.indexOf("GET /left") >= 0) {
+              manualactionstate = "left";
+              }
+              else if (header.indexOf("GET /right") >= 0) {
+              manualactionstate = "right";
+              }
+
+            
+            
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #555555;}</style></head>");
+
+            // Web Page Heading
+            client.println("<body><h1>Car controlls</h1>");
+            
+            //bestuuring van de button die bepaald of de auto automatisch of manueel rijd.  
+            client.println("<p>Current drive state: " + drivestate + "</p>");
+            //is de drivestate manual is laat hij de bestuuringsknoppen zien, anders alleen de manual knop      
+            if (drivestate=="automatic") {
+              client.println("<p><a href=\"/manual\"><button class=\"button\">manual</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/automatic\"><button class=\"button button2\">automatic</button></a></p>");
+            
+              client.println("<p>Current action: driving " + manualactionstate + "</p>");
+              
+              if (manualactionstate=="forward") {
+                client.println("<p><a href=\"/stopped\"><button class=\"button\">stop</button></a></p>");
+
+              } else {
+                client.println("<p><a href=\"/forward\"><button class=\"button button2\">forward</button></a></p>");
+                
+              }
+
+              if (manualactionstate=="backward") {
+                client.println("<p><a href=\"/stopped\"><button class=\"button\">stop</button></a></p>");
+
+              } else {
+                client.println("<p><a href=\"/backward\"><button class=\"button button2\">backward</button></a></p>");
+                
+              }
+
+              if (manualactionstate=="left") {
+                client.println("<p><a href=\"/stopped\"><button class=\"button\">stop</button></a>");
+
+              } else {
+                client.println("<p><a href=\"/left\"><button class=\"button button2\">left</button></a>");
+                
+              }
+
+              if (manualactionstate=="right") {
+                client.println("<a href=\"/stopped\"><button class=\"button\">stop</button></a></p>");
+
+              } else {
+                client.println("<a href=\"/right\"><button class=\"button button2\">right</button></a></p>");
+                
+              }
+
+
+
+
+              
+
+            
+            }
+            client.println("</body></html>");
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+  }
+}
--- code/Auto/include/car.h
+++ code/Auto/include/car.h
@@ -4,88 +4,69 @@
 
 class Car {
     private:
-        int DCmotorpin1;    //eerste pin waaraan de DCmotoren worden aangestuurd.
-        int DCmotorpin2;    //tweede pin waaraan de DCmotoren worden aangesloten.
+        int DCmotorpinL1;    //eerste pin waaraan de DCmotoren van de linker kant op worden aangestuurd.
+        int DCmotorpinL2;    //eerste pin waaraan de DCmotoren van de linker kant op worden aangestuurd.
+        int DCmotorpinR1;    //eerste pin waaraan de DCmotoren van de rechter kant op worden aangestuurd.
+        int DCmotorpinR2;    //eerste pin waaraan de DCmotoren van de rechter kant op worden aangestuurd.
+        
         int DCMotorspeedpin;//Pin waarmee de snelheid van de dc motoren word geregeld
-        int servopin;
-
-        int speed;          //waarde die de sneheid representeerd, kan tussen 0-255.
-        int steeringangle;  //waarde die de posititie van de wielen representeerd.
-
+        
         
 
     public:
         
-        Car(int DCMotorpin1, int DCMotorpin2, int DCMotorspeedpin ,int Servopin){
-            //het opgeven van de juiste pinnen voor de motoren en het op 90 graden zetten van de steerangle.
-            DCmotorpin1 = DCMotorpin1;
-            DCmotorpin2 = DCMotorpin2;
-            DCMotorspeedpin = DCMotorspeedpin;
-            servopin = Servopin;
-            steeringangle = 90;
-
+        Car(int DCMotorpinL1, int DCMotorpinL2, int DCMotorpinR1 ,int DCMotorpinR2){
+            DCmotorpinL1 = DCMotorpinL1;
+            DCmotorpinL2 = DCMotorpinL2;
+            DCmotorpinR1 = DCMotorpinR1;
+            DCmotorpinR2 = DCMotorpinR2;
+            
             //De pinnen van de motoren op output zetten.
-            pinMode(DCmotorpin1, OUTPUT);
-            pinMode(DCmotorpin2, OUTPUT);
-            pinMode(DCMotorspeedpin, OUTPUT);
-            pinMode(servopin, OUTPUT);
+            pinMode(DCmotorpinL1, OUTPUT);
+            pinMode(DCmotorpinL2, OUTPUT);
+            pinMode(DCmotorpinR1, OUTPUT);
+            pinMode(DCmotorpinR2, OUTPUT);
 
-            
-
-
-        }
-
-        //Methode voor het veranderen van de snelheid van de auto, de waarde mag maximaal 255 zijn.
-        void SetCarSpeed(int Speed){
-            if (speed > 255){
-                Serial.println("invalid input");
-            }
-            else{
-            speed = Speed;
-            
-            }
-        }
-
-        //methode voor het vooruit laten rijden van de auto.
-        void DriveForward(){
-            digitalWrite(DCmotorpin1, HIGH);
-            digitalWrite(DCmotorpin2, LOW);
-        }
         
-        //Methode voor het achteruit laten rijden van de auto.
-        void DriveBackwards(){
-            digitalWrite(DCmotorpin1, LOW);
-            digitalWrite(DCmotorpin2, HIGH);
         }
 
-        //methode voor het laten stoppen van de auto.
-        void StopDriving(){
-            digitalWrite(DCmotorpin1, LOW);
-            digitalWrite(DCmotorpin2, LOW);
+        void driveForward(){
+            digitalWrite(DCmotorpinL1, HIGH);
+            digitalWrite(DCmotorpinR1, HIGH);
+            digitalWrite(DCmotorpinL2, LOW);
+            digitalWrite(DCmotorpinR2, LOW);
+        }
+
+        void drivebackward(){
+            digitalWrite(DCmotorpinL1, LOW);
+            digitalWrite(DCmotorpinR1, LOW);
+            digitalWrite(DCmotorpinL2, HIGH);
+            digitalWrite(DCmotorpinR2, HIGH);
+        }
+
+        void turnright(){
+            digitalWrite(DCmotorpinL1, LOW);
+            digitalWrite(DCmotorpinR1, HIGH);
+            digitalWrite(DCmotorpinL2, HIGH);
+            digitalWrite(DCmotorpinR2, LOW);
+        }
+
+        void turnleft(){
+            digitalWrite(DCmotorpinL1, HIGH);
+            digitalWrite(DCmotorpinR1, LOW);
+            digitalWrite(DCmotorpinL2, LOW);
+            digitalWrite(DCmotorpinR2, HIGH);
+        }
+
+        void standStill(){
+            digitalWrite(DCmotorpinL1, LOW);
+            digitalWrite(DCmotorpinR1, LOW);
+            digitalWrite(DCmotorpinL2, LOW);
+            digitalWrite(DCmotorpinR2, LOW);
         }
 
 
-
-        //methode voor het instellen van de wielen in een opgegeven hoek.
-        void SteeringWheelAngle(int angle){
-            long int pwm = (angle*10.6) + 460;      // Convert angle to microseconds
-            for (angle = 0; angle <= 5; angle += 1)  {
-                digitalWrite(servopin, HIGH);
-                delayMicroseconds(pwm);
-                digitalWrite(servopin, LOW);
-                delay(40);                   // Refresh cycle of servo
-            }
-            steeringangle = angle;
-        }
-
-        //methode om de servo 1 graden naar rechts te laten draaien.
-        void SteerRight(){
-            SteeringWheelAngle(++steeringangle);
-        }
-
-        //methode om de servo 1 graden naar links te laten draaien.
-        void SteerLeft(){
-            SteeringWheelAngle(--steeringangle);
-        }
+        
+        
 };
 
